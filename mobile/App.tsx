849cebdea0dc48887910f44cc3a02cdd4c955e41
Guilherme Franco <9uifranco@gmail.com>
2023-01-18 23:23:02 -0300

    Remove nested git repo

---
+++ mobile/App.tsx
@@ -0,0 +1,46 @@
+import { Button, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+    return (
+      <Loading/>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+      <Button
+        title="Hello"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    margin: 20,
+    color: '#fff',
+    fontFamily: 'Inter_800ExtraBold'
+  }
+});
