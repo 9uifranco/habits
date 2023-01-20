2d024acb90f1b7ec18e1bc87fcb4d54094e57eb1
Guilherme Franco <9uifranco@gmail.com>
2023-01-19 23:48:40 -0300

    Add Header and HabitDay component and also Home screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
-import { Button, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 
 export default function App() {
@@ -19,28 +20,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+    <>
+      <Home/>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      <Button
-        title="Hello"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    margin: 20,
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
