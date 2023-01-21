af8a206b35dad7e0330a1bee6e563881f7c5405e
Guilherme Franco <9uifranco@gmail.com>
2023-01-21 10:14:31 -0300

    Create popover for check day progress

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,10 +1,29 @@
+import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
+
+/*
 interface HabitDayProps {
     completed: number
 }
+*/
 
 function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 rounded-lg border-zinc-800"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 rounded-lg border-zinc-800">
+        
+        <Popover.Portal>
+          <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+            <span className='font-semibold text-zinc-400'>Terça-feira</span>
+            <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+
+            <ProgressBar progress={20}/>
+
+            <Popover.Arrow className='fill-zinc-900 h-5 w-10'/>
+          </Popover.Content>  
+        </Popover.Portal>
+      </Popover.Trigger>
+    </Popover.Root>
   )
 }
 
